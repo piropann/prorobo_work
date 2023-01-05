ef653e05fa07868da5f69e93b6170cc128cb15e2
Hirofumi Saito <tasogare1623@gmail.com>
2023-01-05 15:45:18 +0900

    compare fin

--- compare.py
+++ compare.py
@@ -4,20 +4,29 @@ from scipy import stats
 
 a = 0.9
 b = 0.6
-n = 5
-k = np.arange(0, n+1)
+n = 20
 
-x1 = np.linspace(0, 1, n+1)
-print(x1)
-binom_pmf = stats.binom.pmf(k, n, p)
-print(binom_pmf)
+for i in range(n):
+    k = np.arange(0, n+1)
+    x1 = np.linspace(0, 1, n+1)
+    #print(x1)
+    binom_pmf_before = stats.binom.pmf(k, n, b)
+    #print(binom_pmf)
+
+for i in range(n):
+    k = np.arange(0, n+1)
+    x2 = np.linspace(0, 1, n+1)
+    #print(x2)
+    binom_pmf_after = stats.binom.pmf(k, n, a)
+    #print(binom_pmf)
 #y1 = np.linspace(0.01, 0.01, 101)
 
 plt.title("比較")
 plt.xlabel("t")
 plt.ylabel("y")
 
-plt.plot(x1, binom_pmf, color = "red")
+plt.plot(x1, binom_pmf_before, color = "red")
+plt.plot(x2, binom_pmf_after, color = "blue")
 #plt.plot(x2, y2, marker = "A", color = "blue")
 
 plt.show()
