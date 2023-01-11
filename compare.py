a31668c6a5c50c37fdb85bfe2bbef12e82c6cd0f
Hirofumi Saito <tasogare1623@gmail.com>
2023-01-11 14:05:28 +0900

    last

--- compare.py
+++ compare.py
@@ -4,29 +4,31 @@ from scipy import stats
 
 a = 0.9
 b = 0.6
-n = 20
+n = [1,5,10,20,50,100]
 
-for i in range(n):
-    k = np.arange(0, n+1)
-    x1 = np.linspace(0, 1, n+1)
+k = np.arange(0. , 1.01, 0.01)
+
+
+for i in range(6):
+    k = np.arange(0, n[i]+1)
+    x1 = np.linspace(0, 1, n[i]+1)
     #print(x1)
-    binom_pmf_before = stats.binom.pmf(k, n, b)
+    plt.subplot(4,3,i+1)
+    binom_pmf_before = stats.binom.pmf(k, n[i], b)
+    plt.plot(x1, binom_pmf_before, color = "red")
     #print(binom_pmf)
 
-for i in range(n):
-    k = np.arange(0, n+1)
-    x2 = np.linspace(0, 1, n+1)
+for i in range(6):
+    k = np.arange(0, n[i]+1)
+    x2 = np.linspace(0, 1, n[i]+1)
     #print(x2)
-    binom_pmf_after = stats.binom.pmf(k, n, a)
+    plt.subplot(4,3,i+1)
+    binom_pmf_after = stats.binom.pmf(k, n[i], a)
+    plt.plot(x2, binom_pmf_after, color = "blue")
     #print(binom_pmf)
-#y1 = np.linspace(0.01, 0.01, 101)
 
 plt.title("比較")
 plt.xlabel("t")
 plt.ylabel("y")
-
-plt.plot(x1, binom_pmf_before, color = "red")
-plt.plot(x2, binom_pmf_after, color = "blue")
-#plt.plot(x2, y2, marker = "A", color = "blue")
 
 plt.show()
